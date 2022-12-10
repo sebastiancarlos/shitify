f1710f1967122638d15acaa7fd1c19766fca0ba6
Sebastian Carlos <sebastiancarlos@gmail.com>
2022-12-11 00:55:00 +0100

    fix github link

--- src/GithubLogo.tsx
+++ src/GithubLogo.tsx
@@ -1,7 +1,7 @@
 export const GithubLogo = () => {
   return (
     <a
-      href="https://github.com/sebastiancarlos/chicken-ipsum"
+      href="https://github.com/sebastiancarlos/shitify.js"
       rel="noopener noreferrer"
       target="_blank"
       className="github-link"
